93633fc62d8c8a3cf93ed39c4df4964ee7705fcb
Rodrigo k sturm <rodrigoks@gmail.com>
2018-03-01 14:03:41 -0300

    Projeto adivinha

--- Adivinha/app/src/main/java/br/com/jbusiness/adivinha/MainActivity.java
+++ Adivinha/app/src/main/java/br/com/jbusiness/adivinha/MainActivity.java
@@ -2,12 +2,33 @@ package br.com.jbusiness.adivinha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button jogar;
+    private TextView resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jogar = findViewById(R.id.button_play_id);
+        resultado = findViewById(R.id.show_resultado_id);
+
+        jogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random ram = new Random();
+                int randomValue = ram.nextInt();
+                resultado.setText("O valor gerado é: " + randomValue);
+            }
+        });
+
     }
 }
