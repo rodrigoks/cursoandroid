203d501d6ac50c6a47df377f6874e1395859b9ca
Rodrigo k sturm <rodrigoks@gmail.com>
2018-03-05 18:53:14 -0300

    Implementado aplicativo cara ou coroa.

--- CaraouCoroa/app/src/main/java/br/com/jbusiness/caraoucoroa/MainActivity.java
+++ CaraouCoroa/app/src/main/java/br/com/jbusiness/caraoucoroa/MainActivity.java
@@ -1,13 +1,48 @@
 package br.com.jbusiness.caraoucoroa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView btnJogar;
+    private String[] opcoes = { "cara", "coroa" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnJogar = findViewById(R.id.iv_btn_jogar_id);
+
+        btnJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /*
+                 * Preparando para gerar numero aleatorio entre 0 e 1
+                 * que sera utilizado como indice para recuperar a opcao
+                 * da moeda.
+                 */
+                Random randNumber = new Random();
+                int numeroAleatorio = randNumber.nextInt(2);
+
+                /*
+                 * Prepara informações e chamada para proxima activity
+                 */
+                Intent intSecond = new Intent(MainActivity.this, SecondActivity.class);
+                intSecond.putExtra("opcao", opcoes[numeroAleatorio]);
+
+                /*
+                 * Chamar a proxima activity
+                 */
+                startActivity(intSecond);
+            }
+        });
     }
 }
--- CaraouCoroa/app/src/main/java/br/com/jbusiness/caraoucoroa/SecondActivity.java
+++ CaraouCoroa/app/src/main/java/br/com/jbusiness/caraoucoroa/SecondActivity.java
@@ -1,13 +1,44 @@
 package br.com.jbusiness.caraoucoroa;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private ImageView ivMoeda;
+    private ImageView ivResultado;
+    private ImageView btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        ivMoeda = findViewById(R.id.iv_resultado_id);
+        btnVoltar = findViewById(R.id.iv_btn_voltar_id);
+        ivResultado = findViewById(R.id.iv_resultado_id);
+
+        Bundle extras = getIntent().getExtras();
+
+        if(extras != null){
+
+            String opcao = extras.getString("opcao");
+            if("cara".equals(opcao)){
+                ivResultado.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.moeda_cara));
+            } else {
+                ivResultado.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.moeda_coroa));
+            }
+        }
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SecondActivity.this, MainActivity.class));
+            }
+        });
     }
 }
