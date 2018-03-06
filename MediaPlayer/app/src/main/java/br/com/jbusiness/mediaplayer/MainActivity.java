4722c857c11244ddd9facc8762bdc0558da15db3
Rodrigo k sturm <rodrigoks@gmail.com>
2018-03-06 16:58:49 -0300

    Implementado projeto media player.

--- MediaPlayer/app/src/main/java/br/com/jbusiness/mediaplayer/MainActivity.java
+++ MediaPlayer/app/src/main/java/br/com/jbusiness/mediaplayer/MainActivity.java
@@ -1,13 +1,66 @@
 package br.com.jbusiness.mediaplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnPlay;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPlay = findViewById(R.id.btn_play_id);
+        mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.musica);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer.isPlaying())
+                    pause();
+                else
+                    play();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(mediaPlayer != null){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        super.onDestroy();
+    }
+
+    /**
+     * executar a musica
+     */
+    private void play() {
+
+        if(mediaPlayer != null) {
+            mediaPlayer.start();
+            btnPlay.setText("Pausar");
+        }
+
+    }
+
+    /**
+     * pausar a musica
+     */
+    private void pause() {
+
+        if(mediaPlayer != null) {
+            mediaPlayer.pause();
+            btnPlay.setText("Play");
+        }
+
     }
 }
